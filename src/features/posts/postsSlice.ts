ecc5ae5263d1ccdff75f3d9a16041d1e3e9895c1
Vitor Hugo <vitork42@gmail.com>
2023-03-07 23:22:52 -0300

    first commit

---
+++ src/features/posts/postsSlice.ts
@@ -0,0 +1,52 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit"
+import { sub } from "date-fns"
+import { RootState } from "../../app/store"
+
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
+  
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, {payload}) {
+        state.posts.push(payload)
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            title, 
+            content,
+            date: new Date().toISOString(),
+            userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0
+          }
+          }
+        }
+      }
+    },
+    reactionAdded(state, {payload}) {
+      const { postId, reaction }: { postId: string, reaction: any } = payload
+      const existingPost = state.posts.find(post => post.id === postId)
+      if (existingPost) {
+          existingPost.reactions[reaction]++
+      }
+  }
+  }
+})
+
+export const selectAllPosts = (state:RootState) => state.posts.posts
+
+export const { postAdded, reactionAdded } = postsSlice.actions
+export const postsReducer = postsSlice.reducer
